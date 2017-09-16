3c97f978ddf4673ac428851e32623665cb1e2d23
yolorusher <yolorusher@gmail.com>
2017-09-16 19:34:04 +0300

    Lab1 Models

---
+++ Shaykhullin.Models/Lab1/Quaternion.cs
@@ -0,0 +1,73 @@
+﻿namespace Shaykhullin.Models.Lab1
+{
+  public class Quaternion
+  {
+    public static QuaternionFactory Factory { get; } = new QuaternionFactory();
+
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Degree { get; set; }
+
+    private Quaternion(double x, double y) : this(x, y, 1.0)
+    {
+    }
+    private Quaternion(double x, double y, double degree)
+    {
+      X = x;
+      Y = y;
+      Degree = degree;
+    }
+
+    public static Quaternion operator +(Quaternion a, Quaternion b)
+    {
+      if (a.Degree == b.Degree)
+      {
+        return Factory.WithDegree(a.X + b.X, a.Y + b.Y, a.Degree);
+      }
+
+      a.X /= a.Degree;
+      a.Y /= a.Degree;
+      b.X /= b.Degree;
+      b.Y /= b.Degree;
+      return Factory.With(a.X + b.X, a.Y + b.Y);
+    }
+    public static Quaternion operator -(Quaternion a, Quaternion b)
+    {
+      if (a.Degree == b.Degree)
+      {
+        return Factory.WithDegree(a.X - b.X, a.Y - b.Y, a.Degree);
+      }
+
+      a.X /= a.Degree;
+      a.Y /= a.Degree;
+      b.X /= b.Degree;
+      b.Y /= b.Degree;
+      return Factory.With(a.X - b.X, a.Y - b.Y);
+    }
+    public static Quaternion operator *(Quaternion a, Quaternion b)
+    {
+      if (a.Degree == b.Degree)
+      {
+        return Factory.WithDegree(a.X * b.X, a.Y * b.Y, a.Degree);
+      }
+
+      a.X /= a.Degree;
+      a.Y /= a.Degree;
+      b.X /= b.Degree;
+      b.Y /= b.Degree;
+      return Factory.With(a.X * b.X, a.Y * b.Y);
+    }
+
+    public class QuaternionFactory
+    {
+      public Quaternion With(double x, double y)
+      {
+        return new Quaternion(x, y);
+      }
+      public Quaternion WithDegree(double x, double y, double degree)
+      {
+        return new Quaternion(x, y, degree);
+      }
+    }
+  }
+}
