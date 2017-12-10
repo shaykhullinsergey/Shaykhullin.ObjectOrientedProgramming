71907a117afbffccaf97af0daf7678ab087a4eee
Shaykhullin <yolorusher@gmail.com>
2017-12-10 20:19:10 +0300

    Лексема с маленькой

--- Shaykhullin.Lab4/Shaykhullin.Lab4/LexemeSorters/PlusOperationLexemeSorter.cs
+++ Shaykhullin.Lab4/Shaykhullin.Lab4/LexemeSorters/PlusOperationLexemeSorter.cs
@@ -6,7 +6,7 @@ namespace Shaykhullin.LexemeSorters
 {
   public class PlusOperationLexemeSorter : LexemeSorter<PlusLexeme>
   {
-    public override void Sort(Lexeme Lexeme, Lexeme prevLexeme, Queue<Lexeme> input, Queue<Lexeme> output, Stack<Lexeme> stack)
+    public override void Sort(Lexeme lexeme, Lexeme prevLexeme, Queue<Lexeme> input, Queue<Lexeme> output, Stack<Lexeme> stack)
     {
       if(!(prevLexeme is ValueLexeme || prevLexeme is RightParenthesisLexeme))
       {
@@ -16,13 +16,13 @@ namespace Shaykhullin.LexemeSorters
       {
         while (stack.Count > 0 && stack.Peek() is OperationLexeme)
         {
-          if (stack.Peek().Order >= Lexeme.Order)
+          if (stack.Peek().Order >= lexeme.Order)
             break;
 
           output.Enqueue(stack.Pop());
         }
 
-        stack.Push(Lexeme);
+        stack.Push(lexeme);
       }
     }
   }
