625979281fe6a80d348d2f243b198f61646acdbc
Shaykhullin <yolorusher@gmail.com>
2018-01-10 23:18:43 +0300

    no message

---
+++ Shaykhullin.Lab6/Commands/EnterBracketCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shaykhullin.Lab6.Commands
+{
+	public class EnterBracketCommand : Command
+	{
+		public override Keys Key => Keys.Enter;
+
+		public override void Apply(RichTextBox code)
+		{
+			var selected = code.SelectionStart;
+			if(code.Text.Length != selected 
+			   && code.Text[selected - 1] == '{' 
+			   && code.Text[selected] == '}')
+			{
+				Task.Run(() =>
+				{
+					CodeEditor.LockWindowUpdate(code.Handle);
+					Thread.Sleep(70);
+					code.Text = code.Text.Insert(selected + 1, "\t\n");
+					code.SelectionStart = selected + 2;
+					CodeEditor.LockWindowUpdate(IntPtr.Zero);
+				});
+			}
+		}
+	}
+}
